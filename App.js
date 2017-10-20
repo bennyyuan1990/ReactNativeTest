0fcdedf6948ceb680de59c0750d9c10e767f892a
benny <bennyyuan1990@sina.com>
2017-10-20 13:51:23 +0800

    test

--- App.js
+++ App.js
@@ -4,62 +4,113 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Platform,
   StyleSheet,
   Text,
   View,
   TouchableOpacity,
-  TouchableHighlight
-} from 'react-native';
+  TouchableHighlight,
+  Picker,
+  Button,
+  ViewPagerAndroid
+} from "react-native";
+import { StackNavigator } from "react-navigation";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
-export default class App extends Component<{}> {
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       language: null
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <ViewPagerAndroid style={styles.viewPager} initialPage={0}>
+//         <View style={styles.pageStyle}>
+//           <Text>First page</Text>
+//         </View>
+//         <View style={styles.pageStyle}>
+//           <Text>Second page</Text>
+//         </View>
+//       </ViewPagerAndroid>
+//     );
+//   }
+// }
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: "Welcome"
+  };
   render() {
+    const { navigate } = this.props.navigation;
+
     return (
-      <View style={styles.container}>
-        <TouchableHighlight activeOpacity={0.9} underlayColor="#ff0000" onPress={()=>{}}>
-          <Text style={styles.welcome}>
-            Welcome to React Native!
-          </Text>
-        </TouchableHighlight>
-        <TouchableHighlight activeOpacity={0.8} underlayColor="red">
-          <Text style={styles.instructions}>
-            To get started, edit App.js
-          </Text>
-        </TouchableHighlight>
-        <TouchableOpacity activeOpacity={0.8}>
-          <Text style={styles.instructions}>
-            {instructions}
-          </Text>
-        </TouchableOpacity>
+      <View>
+        <Text>Hello, Chat App!</Text>
+        <Button onPress={() => navigate("Chat")} title="Chat with Lucy" />
       </View>
     );
   }
 }
 
+class ChatScreen extends React.Component {
+  static navigationOptions = {
+    title: "Chat with Lucy"
+  };
+  render() {
+    return (
+      <View>
+        <Text>Chat with Lucy</Text>
+      </View>
+    );
+  }
+}
+ const AppNavigator = StackNavigator({
+  Home: { screen: HomeScreen },
+  Chat: { screen: ChatScreen }
+});
+
+export default class App extends Component {
+  render() {
+    return <AppNavigator />;
+  }
+}
+
 const styles = StyleSheet.create({
-  container: {
+  viewPager: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    // justifyContent: "center",
+    // alignItems: "center",
+    justifyContent: "space-around",
+    backgroundColor: "#ff0000"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
   },
+  picker: {
+    color: "#444444",
+    height: 30,
+    backgroundColor: "green"
+  },
+  pageStyle: {
+    alignItems: "center",
+    padding: 20
+  }
 });
